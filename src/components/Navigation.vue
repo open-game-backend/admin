da79715be4b672c7866a9d5c334d6880e8596d7b
Nick Prühs <dev@npruehs.de>
2020-12-10 19:10:00 +0100

    Add icons to navigation bar.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,16 +2,16 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <router-link to="/servers" class="nav-link" v-bind:class="{ active: isRoute('/servers') }">Servers</router-link>
+                <router-link to="/servers" class="nav-link" v-bind:class="{ active: isRoute('/servers') }"><i class="fas fa-server"></i> Servers</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="/queue" class="nav-link" v-bind:class="{ active: isRoute('/queue') }">Queue</router-link>
+                <router-link to="/queue" class="nav-link" v-bind:class="{ active: isRoute('/queue') }"><i class="fas fa-user-friends"></i> Queue</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="/admins" class="nav-link" v-bind:class="{ active: isRoute('/admins') }">Admins</router-link>
+                <router-link to="/admins" class="nav-link" v-bind:class="{ active: isRoute('/admins') }"><i class="fas fa-users-cog"></i> Admins</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="/logout" class="nav-link" v-bind:class="{ active: isRoute('/logout') }">Logout</router-link>
+                <router-link to="/logout" class="nav-link" v-bind:class="{ active: isRoute('/logout') }"><i class="fas fa-sign-out-alt"></i> Logout</router-link>
             </li>
 
             <li>
