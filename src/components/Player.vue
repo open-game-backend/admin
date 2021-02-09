29f93d3d6f54882f18401d945a93837fe08fc973
Nick Prühs <dev@npruehs.de>
2021-02-09 23:12:47 +0100

    Allow changing item counts in player collections.

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -40,9 +40,21 @@
 
                     <tr v-for="item in collection" :key="item.id">
                         <th scope="row">{{ item.id }}</th>
-                        <td scope="row">{{ item.count }}</td>
-                        <td scope="row">
-                            <button type="button" title="Remove" v-on:click="removeItem(item)" class="btn btn-secondary btn-sm"><i class="fas fa-trash"></i></button>
+                        
+                        <td scope="row" v-if="selectedItem.itemDefinitionId === item.id">
+                            <input type="number" min="1" v-model="selectedItem.itemCount" />
+                        </td>
+                        <td scope="row" v-else>
+                            {{ item.count }}
+                        </td>
+                        
+                        <td scope="row" v-if="selectedItem.itemDefinitionId === item.id">
+                            <button type="button" title="Accept" v-on:click="saveItem(item)" class="btn btn-secondary btn-sm"><i class="fas fa-check"></i></button>
+                            <button type="button" title="Cancel" v-on:click="cancel()" class="btn btn-secondary btn-sm"><i class="fas fa-times"></i></button>
+                        </td>
+                        <td scope="row" v-else>
+                            <button type="button" title="Edit" v-on:click="editItem(item)" class="btn btn-secondary btn-sm"><i class="fas fa-edit"></i></button>
+                            <button type="button" title="Remove" v-on:click="removeItem(item)" class="btn btn-secondary btn-sm"><i class="fas fa-trash"></i></button>                           
                         </td>
                     </tr>
                 </tbody>
@@ -53,43 +65,49 @@
 
 <script>
 export default {
-  name: 'Player',
+    name: 'Player',
 
-  data: function() {
-    return {
-        collection: [],
-        itemDefinitions: [],
-        newItem: {
-            itemDefinitionId: "",
-            itemCount: 1
+    data: function() {
+        return {
+            collection: [],
+            itemDefinitions: [],
+            newItem: {
+                itemDefinitionId: "",
+                itemCount: 1
+            },
+            selectedItem: {
+                itemDefinitionId: null,
+                itemCount: 0
+            }
         }
-    }
-  },
+    },
 
-  created: function () {
-    this.getData();
-  },
+    created: function () {
+        this.getData();
+    },
 
-  computed: {
-    playerId () {
-        return this.$route.params.playerId
-    }
-  },
+    computed: {
+        playerId () {
+            return this.$route.params.playerId
+        }
+    },
 
-  methods: {
-      getData: function () {
-        this.$api.get('/open-game-backend-collection/admin/collection/' + this.playerId,
-            response => {
-                this.collection = response.data.collection;
-            });
-        
-        this.$api.get('/open-game-backend-collection/admin/itemdefinitions',
-            response => {
-                this.itemDefinitions = response.data.itemDefinitions;
-            });
-      },
+    methods: {
+        getData: function () {
+            this.$api.get('/open-game-backend-collection/admin/collection/' + this.playerId,
+                response => {
+                    this.collection = response.data.collection;
 
-      addItem: function () {
+                    console.log(this.collection[0].id)
+                });
+            
+            this.$api.get('/open-game-backend-collection/admin/itemdefinitions',
+                response => {
+                    this.itemDefinitions = response.data.itemDefinitions;
+                });
+        },
+
+        addItem: function () {
             if (this.newItem.itemDefinitionId == "") {
                 return;
             }
@@ -97,14 +115,33 @@ export default {
             this.$api.post('/open-game-backend-collection/admin/collection/' + this.playerId + '/items', this.newItem,
                 () => { this.getData(); }
             );
-      },
+        },
 
-      removeItem: function (item) {
+        editItem: function(item) {
+            this.selectedItem.itemDefinitionId = item.id
+            this.selectedItem.itemCount = item.count
+        },
+
+        saveItem: function() {
+            this.$api.put('/open-game-backend-collection/admin/collection/' + this.playerId + '/items/' + this.selectedItem.itemDefinitionId,
+                { 'itemCount': this.selectedItem.itemCount },
+                () => { 
+                    this.getData();
+                    this.cancel();
+                }
+            );
+        },
+
+        cancel: function() {
+            this.selectedItem.itemDefinitionId = null
+        },
+
+        removeItem: function (item) {
             this.$api.delete('/open-game-backend-collection/admin/collection/' + this.playerId + '/items/' + item.id,
                 () => { this.getData(); }
             );
-      }
-  }
+        }
+    }
 }
 </script>
 
